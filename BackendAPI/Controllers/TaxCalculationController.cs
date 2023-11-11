1bd5e2ee5345ca20c949f4d5c1adeb1274c2b546
stefan <stefan.labuschagne@gmail.com>
2023-11-11 10:20:12 +0200

    Add project files.

---
+++ BackendAPI/Controllers/TaxCalculationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackendAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class TaxCalculationController : Controller
+	{
+
+		private readonly ILogger<TaxCalculationController> _logger;
+
+		public TaxCalculationController(ILogger<TaxCalculationController> logger)
+		{
+			_logger = logger;
+		}
+
+
+		[HttpGet(Name = "GetWeatherForecast")]
+		public IEnumerable<WeatherForecast> Get()
+		{
+			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			{
+				Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+				TemperatureC = Random.Shared.Next(-20, 55),
+				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+			})
+			.ToArray();
+		}
+	}
+}
