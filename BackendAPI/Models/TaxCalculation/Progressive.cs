ee94bada23c95c7120d2f0c71d43bf3461fcd552
stefan <stefan.labuschagne@gmail.com>
2023-11-12 19:32:50 +0200

    Updated Progressive tax method

--- BackendAPI/Models/TaxCalculation/Progressive.cs
+++ BackendAPI/Models/TaxCalculation/Progressive.cs
@@ -14,19 +14,19 @@ public class Progressive : ITaxCalculation
         return ((_taxableamount - 372950) * 0.35m) + 108216;
 
       if (_taxableamount >= 171551)
-        return _taxableamount - (_taxableamount - 171550) * 0.33m + 41754;
+        return ((_taxableamount - 171550) * 0.33m) + 41754;
 
       if (_taxableamount >= 82251)
-        return _taxableamount - (_taxableamount - 82250) * 0.28m + 16750;
+        return ((_taxableamount - 82250) * 0.28m) + 16750;
 
       if (_taxableamount >= 33951)
-        return _taxableamount - (_taxableamount - 33950) * 0.25m + 4675;
+        return ((_taxableamount - 33950) * 0.25m) + 4675;
 
       if (_taxableamount >= 8351)
-        return _taxableamount - (_taxableamount - 8350) * 0.15m + 835;
+        return ((_taxableamount - 8350) * 0.15m) + 835;
 
       if (_taxableamount >= 0)
-        return _taxableamount - _taxableamount * 0.10m + 0;
+        return (_taxableamount * 0.10m) + 0;
 
       return 0;
     }
