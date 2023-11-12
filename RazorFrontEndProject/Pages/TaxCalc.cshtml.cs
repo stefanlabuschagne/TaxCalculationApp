2adf58d5b58a6248f63a9bc8c9b094f74efa0f87
stefan <stefan.labuschagne@gmail.com>
2023-11-12 17:10:46 +0200

    Added TaxInfo Model

--- RazorFrontEndProject/Pages/TaxCalc.cshtml.cs
+++ RazorFrontEndProject/Pages/TaxCalc.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorFrontEndProject.Models;
+using System.Net.Http;
 
 namespace RazorFrontEndProject.Pages
 {
@@ -12,13 +13,41 @@ namespace RazorFrontEndProject.Pages
 
       }
 
-    public void OnPost(TaxInformation TaxInfoModel)
+    public async void OnPost(TaxInformation TaxInfoModel)
     {
       // Logic for handling POST requests
       // Access posted values using the Request.Form collection or model binding
 
 
       // Call API to update the database
+      // Create an instance of HttpClient
+      using (HttpClient client = new HttpClient())
+      {
+        // Set the base address of the API
+        client.BaseAddress = new Uri("http://localhost:5015");
+
+        try
+        {
+          // Make a GET request
+          HttpResponseMessage response = await client.PostAsync("/CalculateTax", TaxInfoModel);
+
+          // Check if the response is successful
+          if (response.IsSuccessStatusCode)
+          {
+            // Read and print the response content
+            string result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+          }
+          else
+          {
+            Console.WriteLine($"Error: {response.StatusCode}, {response.ReasonPhrase}");
+          }
+        }
+        catch (HttpRequestException e)
+        {
+          Console.WriteLine($"Request error: {e.Message}");
+        }
+      }
 
     }
   }
